806d418778eeeabd782a9591dc28920bba33feea
Kareem <patankareem03@gmail.com>
2026-08-03 21:48:04 +0530

    upadte

--- program1/p2.py
+++ program1/p2.py
@@ -1,5 +1,9 @@
-a= int(input("enter the value of a"))
-if a % 2 == 0 :
-    print (a, "is not a prime number")
-else :
-    print(a, "is a prime number")
+a = int(input("enter the value of a:"))
+if a <= 1:
+    print (a,"is not a prime number")
+for i in range (2, a):
+    if a % i  == 0:
+        print (a, "is not a prime number")
+        break
+else:
+    print (a, "is a prime number")
